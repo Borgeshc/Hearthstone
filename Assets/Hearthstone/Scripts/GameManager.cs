5a130ec541647a91f0d2975ff0cc07b2a8152a97
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-09-12 14:28:45 -0500

    Spawn players

---
+++ Assets/Hearthstone/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform playerOneHeroPosition;
+    public Transform playerTwoHeroPosition;
+
+    public GameObject playerOne;
+    public GameObject playerTwo;
+
+    private void Start()
+    {
+        CreatePlayers();
+    }
+
+    private void CreatePlayers()
+    {
+        Instantiate(playerOne, playerOneHeroPosition.position, Quaternion.identity, playerOneHeroPosition);
+        Instantiate(playerTwo, playerTwoHeroPosition.position, Quaternion.identity, playerTwoHeroPosition);
+    }
+}
