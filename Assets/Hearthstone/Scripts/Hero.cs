d7f00a69520e0e3f0b9d93dfd15ef13c9877a03c
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-09-12 14:19:20 -0500

    Board set up and health logic

---
+++ Assets/Hearthstone/Scripts/Hero.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Hero : MonoBehaviour
+{
+    public int maxHealth;                                                            
+    public int armor;
+
+    public Text healthText;
+
+    int health;
+
+    private void Start()
+    {
+        health = maxHealth;
+        UpdateTextValues();
+    }
+
+    public void GainHealth(int healthGained)
+    {
+        if (health + healthGained <= maxHealth)
+            health += healthGained;
+        else
+            health = maxHealth;
+
+        UpdateTextValues();
+    }
+
+    public void LoseHealth(int healthLost)
+    {
+        health -= healthLost;
+        UpdateTextValues();
+
+        if (health <= 0)
+            Died();
+    }
+
+    public void GainArmor(int armorGained)
+    {
+
+    }
+
+    public void LoseArmor(int armorLost)
+    {
+
+    }
+
+    private void UpdateTextValues()
+    {
+        healthText.text = health.ToString();
+    }
+
+    void Died()
+    {
+
+    }
+}
