28fce2dd4bd31b7c1b8e344f2b02a78dc965e0e1
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-09-12 20:44:10 -0500

    created mana system base

--- Assets/Hearthstone/Scripts/GameManager.cs
+++ Assets/Hearthstone/Scripts/GameManager.cs
@@ -1,23 +1,77 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Player Variables")]
     public Transform playerOneHeroPosition;
     public Transform playerTwoHeroPosition;
 
     public GameObject playerOne;
     public GameObject playerTwo;
 
+    public GameObject[] manaCrystals;
+
+    Hero playerOneHero;
+    Hero playerTwoHero;
+
+    Hero currentPlayer;
+    Coroutine startTurn;
+
     private void Start()
     {
         CreatePlayers();
+        startTurn = StartCoroutine(StartTurn());
     }
 
     private void CreatePlayers()
     {
-        Instantiate(playerOne, playerOneHeroPosition.position, Quaternion.identity, playerOneHeroPosition);
-        Instantiate(playerTwo, playerTwoHeroPosition.position, Quaternion.identity, playerTwoHeroPosition);
+        playerOneHero = Instantiate(playerOne, playerOneHeroPosition.position, Quaternion.identity, playerOneHeroPosition).GetComponent<Hero>();
+        playerTwoHero = Instantiate(playerTwo, playerTwoHeroPosition.position, Quaternion.identity, playerTwoHeroPosition).GetComponent<Hero>();
+
+        playerOneHero.playerOne = true;
+        playerOneHero.manaCrystals = manaCrystals;
+        currentPlayer = playerOneHero;
     }
+
+    IEnumerator StartTurn()
+    {
+        yield return new WaitForSeconds(1);
+        currentPlayer.StartTurn();
+        yield return new WaitForSeconds(75);
+        currentPlayer.EndTurn();
+
+        if (currentPlayer == playerOneHero)
+            currentPlayer = playerTwoHero;
+        else
+            currentPlayer = playerOneHero;
+    }
+
+    #region TestMethods
+    public void IncreaseMana()
+    {
+        playerOneHero.IncreaseMana(2);
+    }
+
+    public void DecreaseMana()
+    {
+        playerOneHero.DecreaseMana(3);
+    }
+
+    public void IncreaseMaxMana()
+    {
+        playerOneHero.IncreaseMaxMana(2);
+    }
+
+    public void DecreaseMaxMana()
+    {
+        playerOneHero.DecreaseMaxMana(3);
+    }
+
+    public void StartPlayerTurn()
+    {
+        playerOneHero.StartTurn();
+    }
+    #endregion
 }
--- Assets/Hearthstone/Scripts/Hero.cs
+++ Assets/Hearthstone/Scripts/Hero.cs
@@ -3,8 +3,22 @@ using UnityEngine.UI;
 
 public class Hero : MonoBehaviour
 {
+    [Header("Mana Crystals")]
+    public GameObject[] manaCrystals;
+    public Text currentManaText;
+    public Text maxManaText;
+
+    int currentMana;
+    int maxMana;
+
+    [Space, Header("Stats")]
     public int maxHealth;                                                            
     public int armor;
+
+    [HideInInspector]
+    public bool playerOne;
+    [HideInInspector]
+    public bool myTurn;
 
     public Text healthText;
 
@@ -13,9 +27,32 @@ public class Hero : MonoBehaviour
     private void Start()
     {
         health = maxHealth;
-        UpdateTextValues();
+        SetReferences();
+        UpdateHealthValues();
+        UpdateManaCrystals();
     }
 
+    private void SetReferences()
+    {
+        if(playerOne)
+        {
+            currentManaText = GameObject.Find("PlayerOneCurrentMana").GetComponent<Text>();
+            maxManaText = GameObject.Find("PlayerOneMaxMana").GetComponent<Text>();
+
+            foreach (GameObject go in manaCrystals)
+            {
+                go.SetActive(false);
+            }
+
+            manaCrystals[0].SetActive(true);
+        }
+        else
+        {
+            currentManaText = GameObject.Find("PlayerTwoCurrentMana").GetComponent<Text>();
+            maxManaText = GameObject.Find("PlayerTwoMaxMana").GetComponent<Text>();
+        }
+    }
+    #region Health
     public void GainHealth(int healthGained)
     {
         if (health + healthGained <= maxHealth)
@@ -23,18 +60,24 @@ public class Hero : MonoBehaviour
         else
             health = maxHealth;
 
-        UpdateTextValues();
+        UpdateHealthValues();
     }
 
     public void LoseHealth(int healthLost)
     {
         health -= healthLost;
-        UpdateTextValues();
+        UpdateHealthValues();
 
         if (health <= 0)
             Died();
     }
 
+    private void UpdateHealthValues()
+    {
+        healthText.text = health.ToString();
+    }
+    #endregion
+    #region Armor
     public void GainArmor(int armorGained)
     {
 
@@ -44,13 +87,106 @@ public class Hero : MonoBehaviour
     {
 
     }
-
-    private void UpdateTextValues()
+    #endregion
+    #region Mana
+    public void IncreaseMana(int increaseAmount)
     {
-        healthText.text = health.ToString();
+        for (int i = 0; i < increaseAmount; i++)
+        {
+            if (currentMana + 1 <= maxMana)
+                currentMana ++;
+            else
+                currentMana = maxMana;
+
+            UpdateManaCrystals();
+
+            if (playerOne)
+            {
+                if (currentMana > 0)
+                    manaCrystals[currentMana - 1].GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, .75f);
+            }
+        }
     }
 
-    void Died()
+    public void DecreaseMana(int decreaseAmount)
+    {
+        for (int i = 0; i < decreaseAmount; i++)
+        {
+            if (currentMana - 1 >= 0)
+                currentMana--;
+            else
+                currentMana = 0;
+
+            UpdateManaCrystals();
+
+            if (playerOne)
+            {
+                if (currentMana > 0)
+                    manaCrystals[currentMana].GetComponent<SpriteRenderer>().color = new Color(.5f, .5f, .5f, .75f);
+                else
+                    manaCrystals[0].GetComponent<SpriteRenderer>().color = new Color(.5f, .5f, .5f, .75f);
+            }
+        }
+    }
+
+    public void IncreaseMaxMana(int increaseAmount)
+    {
+        for (int i = 0; i < increaseAmount; i++)
+        {
+            if (maxMana + 1 <= 10)
+                maxMana++;
+            else
+                maxMana = 10;
+            UpdateManaCrystals();
+
+            if (playerOne)
+            {
+                if (maxMana > 0)
+                    manaCrystals[maxMana - 1].SetActive(true);
+            }
+        }
+    }
+
+    public void DecreaseMaxMana(int decreaseAmount)
+    {
+        for (int i = 0; i < decreaseAmount; i++)
+        {
+            if (maxMana - 1 >= 0)
+                maxMana--;
+            else
+                maxMana = 0;
+
+            UpdateManaCrystals();
+
+            if (playerOne)
+            {
+                if (maxMana > 0)
+                    manaCrystals[maxMana - 1].SetActive(false);
+            }
+        }
+    }
+
+    private void UpdateManaCrystals()
+    {
+        print(playerOne);
+        currentManaText.text = currentMana.ToString();
+        maxManaText.text = maxMana.ToString();
+    }
+    #endregion
+
+    public void StartTurn()
+    {
+        myTurn = true;
+        IncreaseMaxMana(1);
+        IncreaseMana(10);
+    }
+
+    public void EndTurn()
+    {
+        myTurn = false;
+    }
+
+    private void Died()
     {
 
     }
